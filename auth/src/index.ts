989992d2debf527000aa395ce9f28887e46ea913
Shamil Rasheed <shamilrasheed131@gmail.com>
2023-08-13 12:55:36 +0530

    wrote test for signup route and added other dependencies

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,40 +1,5 @@
-import express, { json } from 'express';
-import 'express-async-errors'
-import morgan from 'morgan';
-
-
-
-import { currentUserRouter } from './routes/current-user';
-import { signInRouter } from './routes/signin';
-import { signUpRouter } from './routes/signup';
-import { signOutRouter } from './routes/signout';
-import { errorHandler } from './middlewares/errorHandler';
-import { NotFoundError } from './errors/notFoundError';
+import { app } from './app';
 import { connectDB } from './config/db';
-import cookieSession from 'cookie-session';
 
-const app = express();
-app.set('trust proxy', true); //For express to know req comes through a nginx proxy and it's ok.
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,  //Disable encryption because we are passing jwt
-    secure: true    //use only for https
-  })
-)
-app.use(express.urlencoded({ extended: false, limit:'50mb' }))
-app.use(morgan('dev'));
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signOutRouter);
-app.use(signUpRouter);
-
-app.all('*', () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
 connectDB()
-
 app.listen(3000, () => console.log('Auth listening at 3000!'));
