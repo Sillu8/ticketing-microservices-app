f02a5cdc60b0f09a7285e796131a99a76b21b960
Shamil Rasheed <shamilrasheed131@gmail.com>
2023-08-25 14:58:06 +0530

    added common folder for reusable shared code and edited configuratiions and package.json for common folder

--- client/pages/auth/signout.js
+++ client/pages/auth/signout.js
@@ -2,15 +2,16 @@ import { useEffect } from 'react';
 import useRequest from '../../hooks/useRequest';
 
 const Signout = () => {
-  const {doRequest} = useRequest({
-    url:'/api/users/signout',
+  const { doRequest } = useRequest({
+    url: '/api/users/signout',
     method: 'post',
     body: {},
     onSuccess: () => Router.push('/')
   })
+
   useEffect(() => {
     doRequest();
-   }, [])
+  }, [])
 
   return <div>Signing you out...</div>
 }
